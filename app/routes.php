dd2159b3a920cc05caaae0a1b43f4fa742732c9b
Victor Ernesto <venesto84@gmail.com>
2015-05-11 21:28:23 -0400

    my route testing

--- app/routes.php
+++ app/routes.php
@@ -14,8 +14,44 @@
 //Route::get('/', 'HomeController@index');
 Route::get('downloadfile', 'HomeController@downloadfile');
 Route::get('/', 'UploadController@index');
-Route::post('uploadfiles', ['as' => 'files.upload', 'uses' => 'UploadController@upload'])->before('ban');
-
+Route::group(array('before' => 'ajax|ajaxban'), function () {
+  Route::post('uploadfiles', ['as' => 'files.upload', 'uses' => 'UploadController@upload'])->before('ban');
+});
 //Just a test
 Route::get('/download/{id}', 'UploadController@download');
+
+Route::get('/curltest', function() {
+    $ch = curl_init("http://google.com");
+    // initialize curl handle
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $data = curl_exec($ch);
+    print($data);
+});
+
+Route::get('/utils',function(){
+
+
+    $identity = new \OpenStack\Identity\v2\IdentityService($_ENV['swiftendpoint']);
+
+
+    echo '<h2>identity</h2><pre>';
+    var_dump($identity);
+    echo '</pre>';
+
+    try {
+        $token = $identity->authenticateAsUser($_ENV['swiftusername'], $_ENV['swiftpassword'], null, $_ENV['swifttenantname']);
+
+
+        echo '<h2>token</h2><pre>';
+        var_dump($token);
+        echo '</pre>';
+    }
+    catch(\OpenStack\Common\Exception $ex){
+        echo '<h2>token</h2><pre>';
+        var_dump($ex);
+        echo '</pre>';
+    }
+
+    echo 'finished';
+});
 
