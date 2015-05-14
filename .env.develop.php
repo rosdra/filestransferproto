bb79449fb6d8b1b181a155deb41a9c9161618f26
Victor Ernesto <vernesto84@gmail.com>
2015-05-14 15:03:29 -0400

    env files

---
+++ .env.develop.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 11-05-15
+ * Time: 09:28 AM
+ */
+
+return array(
+    'swiftendpoint'   => 'http://95.110.165.22:35357/v2.0',
+    'swiftusername'   => 'demo',
+    'swiftpassword'   => '5f423b77',
+    'swifttenantname' => 'admin',
+
+    'fileexpirationdays' => 5,
+    'emailsenabled'      => true
+);
