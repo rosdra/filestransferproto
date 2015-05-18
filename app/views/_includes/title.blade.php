f5b9d610e14c8b5a4269c7ec80d16fe2a8d8b11d
rosdra <rosdra@gmail.com>
2015-05-18 16:09:38 -0400

    st

--- app/views/_includes/title.blade.php
+++ app/views/_includes/title.blade.php
@@ -23,3 +23,8 @@
 <!-- bootstrap-dialog -->
 {{ HTML::style('bootstrap-dialog/css/bootstrap-dialog.min.css') }}
 {{ HTML::script('bootstrap-dialog/js/bootstrap-dialog.min.js') }}
+
+{{--New cosas--}}
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+{{ HTML::style('filestransfer/css/app.css') }}
