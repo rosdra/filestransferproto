2f6f1c6ea442eaaaa481b8904a4afaf2f5b12cbe
rosdra <rosdra@gmail.com>
2015-05-18 20:40:26 -0400

    jh

--- app/views/_includes/footer.blade.php
+++ app/views/_includes/footer.blade.php
@@ -5,8 +5,8 @@
 	{{ HTML::script('filestransfer/js/app.js') }}
 
 	<div class="copyright text-right">
-		<a target="_blank" href="http://www.aruba.it">
-            <img id="arubalogo" alt="Aruba" src="{{asset('filestransfer/img/logo_aruba.png')}}">
+		<a target="_blank" href="http://www.aruba.it"> 
+		    {{ HTML::image('filestransfer/img/logo_aruba.png') }}
         </a>
 	</div>
 @show
