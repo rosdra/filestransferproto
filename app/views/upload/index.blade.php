39d3cc604628eaaf851d085f910544a192ab5f59
Raul Fernandez <neo.is.true@gmail.com>
2015-05-18 22:37:29 -0400

    2015-05-18  - UI design v3.5

--- app/views/upload/index.blade.php
+++ app/views/upload/index.blade.php
@@ -10,14 +10,8 @@
     <h3>{{ HTML::image('filestransfer/img/arrow_share_sx.png') }} HOW DO YOU WANT TO SHARE YOUR FILES? {{--{{ HTML::image('filestransfer/img/arrow_share_dx.png') }}--}}</h3>
 </div>
 
-<div id="sharecontainer" class="panel col-md-4 col-sm-4" style="display: none;">
-    <div id="sharezone" class="share">
-        @include('upload._partials.share_by_email')
-    </div>
-    <div id="options" class="row">
-        <button type="button" class="round orange btn-share">SHARE</button>
-        <button type="button" class="round grey btn-share-again" style="display: none"><i class="fa fa-repeat"></i> Share again</button>
-    </div>
+<div id="sharecontainer" class="col-sm-12 col-md-4" style="display: none;">
+    @include('upload._partials.share_by_email')
 </div>
 
 @stop
